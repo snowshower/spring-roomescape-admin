df34640080efae67d29d54c9b890efef180e2842
snowshower <wonsb25@gmail.com>
2026-06-10 22:58:06 +0900

    test: 요구사항 테스트 추가
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- src/test/java/roomescape/RequirementTest.java
+++ src/test/java/roomescape/RequirementTest.java
@@ -3,17 +3,25 @@ package roomescape;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.reservation.controller.ReservationController;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class RequirementTest {
+
+    @Autowired
+    private ReservationController reservationController;
 
     @Test
     void 시간_관리_API() {
@@ -38,7 +46,6 @@ public class RequirementTest {
                 .then().log().all()
                 .statusCode(204);
     }
-
     @Test
     void 예약과_시간_연결() {
         Map<String, Object> time = new HashMap<>();
@@ -70,5 +77,17 @@ public class RequirementTest {
                 .body("size()", is(1));
     }
 
+    @Test
+    void 계층화_리팩터링() {
+        boolean isJdbcTemplateInjected = false;
 
+        for (Field field : reservationController.getClass().getDeclaredFields()) {
+            if (field.getType().equals(JdbcTemplate.class)) {
+                isJdbcTemplateInjected = true;
+                break;
+            }
+        }
+
+        assertThat(isJdbcTemplateInjected).isFalse();
+    }
 }
